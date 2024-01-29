4b4ffb910cbd439fbfb0d53239c66ec2a15ca671
Rawjyot Singh <rawjyot.singh@kearney.com>
2024-01-29 13:07:07 +0530

    Rebase changes

--- src/GlobalComponents/sideBar/Sidebar.jsx
+++ src/GlobalComponents/sideBar/Sidebar.jsx
@@ -1,4 +1,14 @@
-import { FormControl, OutlinedInput, Select } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Autocomplete,
+  FormControl,
+  OutlinedInput,
+  Select,
+  TextField,
+} from "@mui/material";
 import MenuItem from "@mui/material/MenuItem";
 import MenuList from "@mui/material/MenuList";
 import { useState } from "react";
@@ -47,8 +57,23 @@ export default function Sidebar() {
     },
   };
 
+  const AutoData = [
+    { title: "Region One" },
+    { title: "Region Two" },
+    { title: "Region Three" },
+    { title: "Region Four" },
+    { title: "Region Five" },
+  ];
+
+  // For Select
+  const [age, setAge] = useState("");
+
   const toggleRegions = () => {
     setRegionOpen(!regionOpen);
+  };
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
   };
 
   const togglePartner = () => {
@@ -185,6 +210,176 @@ export default function Sidebar() {
                 </NavLink>
               </MenuItem>
             </MenuList>
+          </div>
+          <hr className="mt-3 mb-3 bg-white"></hr>
+          <div className="sidebar-filter">
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1-content"
+                id="panel1-header"
+              >
+                Account Search
+              </AccordionSummary>
+              <AccordionDetails>
+                <div className="filter-control">
+                  <FormControl fullWidth size="small">
+                    <Select
+                      labelId="status"
+                      size="small"
+                      input={<OutlinedInput />}
+                      id="status-select"
+                      value={0}
+                      label="Status"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={0}>Status</MenuItem>
+                      <MenuItem value={10}>Status One</MenuItem>
+                      <MenuItem value={20}>Status Two</MenuItem>
+                    </Select>
+                  </FormControl>
+                </div>
+                <div className="filter-control">
+                  <FormControl fullWidth size="small">
+                    <Select
+                      labelId="status"
+                      id="status-select"
+                      value={0}
+                      input={<OutlinedInput />}
+                      label="Status"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={0}>Industry</MenuItem>
+                      <MenuItem value={10}>Industry One</MenuItem>
+                      <MenuItem value={20}>Industry Two</MenuItem>
+                    </Select>
+                  </FormControl>
+                </div>
+                <div className="filter-control">
+                  <FormControl fullWidth size="small">
+                    <Select
+                      labelId="status"
+                      id="status-select"
+                      input={<OutlinedInput />}
+                      value={0}
+                      label="Status"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={0}>Size One</MenuItem>
+                      <MenuItem value={10}>Size One</MenuItem>
+                      <MenuItem value={20}>Size Two</MenuItem>
+                    </Select>
+                  </FormControl>
+                </div>
+                <div className="filter-control">
+                  <Autocomplete
+                    fullWidth
+                    size="small"
+                    multiple
+                    limitTags={2}
+                    input={<OutlinedInput />}
+                    id="multiple-limit-tags"
+                    options={AutoData}
+                    getOptionLabel={(option) => option.title}
+                    defaultValue={[]}
+                    renderInput={(params) => (
+                      <TextField {...params} placeholder="Region" />
+                    )}
+                    sx={{ width: "200px" }}
+                  />
+                </div>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+          <div className="sidebar-filter">
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1-content"
+                id="panel1-header"
+              >
+                Partner Search
+              </AccordionSummary>
+              <AccordionDetails>
+                <div className="filter-control">
+                  <FormControl fullWidth size="small">
+                    <Select
+                      labelId="status"
+                      input={<OutlinedInput />}
+                      id="status-select"
+                      value={0}
+                      label="Status"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={0}>Partner Name</MenuItem>
+                      <MenuItem value={10}>Partner One</MenuItem>
+                      <MenuItem value={20}>Partner Two</MenuItem>
+                    </Select>
+                  </FormControl>
+                </div>
+                <div className="filter-control">
+                  <FormControl fullWidth size="small">
+                    <Select
+                      labelId="status"
+                      id="status-select"
+                      value={0}
+                      input={<OutlinedInput />}
+                      label="Status"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={0}>Agent</MenuItem>
+                      <MenuItem value={10}>Agent One</MenuItem>
+                      <MenuItem value={20}>Agent Two</MenuItem>
+                    </Select>
+                  </FormControl>
+                </div>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+          <div className="sidebar-filter">
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1-content"
+                id="panel1-header"
+              >
+                Advance Search
+              </AccordionSummary>
+              <AccordionDetails>
+                <div className="filter-control">
+                  <FormControl fullWidth size="small">
+                    <Select
+                      labelId="status"
+                      input={<OutlinedInput />}
+                      id="status-select"
+                      value={0}
+                      label="Status"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={0}>Revenue</MenuItem>
+                      <MenuItem value={10}>Revenue One</MenuItem>
+                      <MenuItem value={20}>Revenue Two</MenuItem>
+                    </Select>
+                  </FormControl>
+                </div>
+                <div className="filter-control">
+                  <FormControl fullWidth size="small">
+                    <Select
+                      labelId="status"
+                      id="status-select"
+                      value={0}
+                      input={<OutlinedInput />}
+                      label="Status"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={0}>Technographics</MenuItem>
+                      <MenuItem value={10}>Technographics One</MenuItem>
+                      <MenuItem value={20}>Technographics Two</MenuItem>
+                    </Select>
+                  </FormControl>
+                </div>
+              </AccordionDetails>
+            </Accordion>
           </div>
         </div>
       </aside>
